ecd9cbeab9e7cc87f6eb73e41f9c7b3dca1467a8
Logan Nommensen <logan.nommensen@gmail.com>
2025-10-02 16:16:40 -0500

    Parse GameData, RPC, and SetColor (WIP)

--- amongus.lua
+++ amongus.lua
@@ -23,23 +23,23 @@ packet_type = {
 -- packet_type = RELIABLE
 
 root_message_type = {
-	HostGame        = 0,
-	JoinGame        = 1,
-	StartGame       = 2,
-	RemoveGame      = 3,
-	RemovePlayer    = 4,
-	GameData        = 5,
-	GameDataTo      = 6,
-	JoinedGame      = 7,
-	EndGame         = 8,
-	GetGameList     = 9,
-	AlterGame       = 10,
-	KickPlayer      = 11,
-	WaitForHost     = 12,
-	Redirect        = 13,
-	ReselectServer  = 14,
-	GetGameListV2   = 16,  -- intentional jump in value
-	ReportPlayer    = 17
+    HostGame        = 0,
+    JoinGame        = 1,
+    StartGame       = 2,
+    RemoveGame      = 3,
+    RemovePlayer    = 4,
+    GameData        = 5,
+    GameDataTo      = 6,
+    JoinedGame      = 7,
+    EndGame         = 8,
+    GetGameList     = 9,
+    AlterGame       = 10,
+    KickPlayer      = 11,
+    WaitForHost     = 12,
+    Redirect        = 13,
+    ReselectServer  = 14,
+    GetGameListV2   = 16,  -- intentional jump in value
+    ReportPlayer    = 17
 }
 
 gameData_type = {
@@ -54,45 +54,60 @@ gameData_type = {
 }
 
 RPC_type = {
-	PlayAnimation     = 0,
-	CompleteTask      = 1,
-	SyncSettings      = 2,
-	SetInfected       = 3,
-	Exiled            = 4,
-	CheckName         = 5,
-	SetName           = 6,
-	CheckColor        = 7,
-	SetColor          = 8,
-	SetHat            = 9,
-	SetSkin           = 10,
-	ReportDeadBody    = 11,
-	MurderPlayer      = 12,
-	SendChat          = 13,
-	StartMeeting      = 14,
-	SetScanner        = 15,
-	SendChatNote      = 16,
-	SetPet            = 17,
-	SetStartCounter   = 18,
-	EnterVent         = 19,
-	ExitVent          = 20,
-	SnapTo            = 21,
-	Close             = 22,
-	VotingComplete    = 23,
-	CastVote          = 24,
-	ClearVote         = 25,
-	AddVote           = 26,
-	CloseDoorsOfType  = 27,
-	RepairSystem      = 28,
-	SetTasks          = 29,
-	ClimbLadder       = 30,
-	UsePlatform       = 31
+    PlayAnimation     = 0,
+    CompleteTask      = 1,
+    SyncSettings      = 2,
+    SetInfected       = 3,
+    Exiled            = 4,
+    CheckName         = 5,
+    SetName           = 6,
+    CheckColor        = 7,
+    SetColor          = 8,
+    SetHat            = 9,
+    SetSkin           = 10,
+    ReportDeadBody    = 11,
+    MurderPlayer      = 12,
+    SendChat          = 13,
+    StartMeeting      = 14,
+    SetScanner        = 15,
+    SendChatNote      = 16,
+    SetPet            = 17,
+    SetStartCounter   = 18,
+    EnterVent         = 19,
+    ExitVent          = 20,
+    SnapTo            = 21,
+    Close             = 22,
+    VotingComplete    = 23,
+    CastVote          = 24,
+    ClearVote         = 25,
+    AddVote           = 26,
+    CloseDoorsOfType  = 27,
+    RepairSystem      = 28,
+    SetTasks          = 29,
+    ClimbLadder       = 30,
+    UsePlatform       = 31
 }
 	
-
-
-
-
-
+player_color = {
+	Red 	= 0,
+	Blue	= 1,
+	Green 	= 2,
+	Pink 	= 3,
+	Orange 	= 4,
+	Yellow 	= 5,
+	Black 	= 6,
+	White 	= 7,
+	Purple 	= 8,
+	Brown 	= 9,
+	Cyan 	= 10,
+	Lime 	= 11,
+	Maroon 	= 12,
+	Rose 	= 13,
+	Banana 	= 14,
+	Gray 	= 15,
+	Tan 	= 16,
+	Coral 	= 17
+}
 
 
 
@@ -107,17 +122,11 @@ message_length_field	= ProtoField.int32("amongus.message_length"		, "messageLeng
 nonce_field 			= ProtoField.int16("amongus.nonce"				, "nonce"			, base.DEC)
 root_message_type_field	= ProtoField.int32("amongus.root_message_type"	, "rootMessageType"	, base.DEC)
 gameID_field			= ProtoField.int32("amongus.gameID" 			, "gameID"			, base.DEC)
-senderID_field			= ProtoField.int32("amongus.senderID"			, "senderID"		, base.DEC)
-gameData_type_field 	= ProtoField.int32("amongus.gameData_type"		, "gameData"		, base.DEC)
-RPC_type_field			= ProtoField.bytes("amongus.rpc_type"			, "RPCtype")
+gameData_type_field 	= ProtoField.int32("amongus.gameData_type" 		, "gameDataType"	, base.DEC)
+RPC_type_field  		= ProtoField.int32("amongus.RPC_type" 			, "RPCType"			, base.DEC)
+player_color_field		= ProtoField.int32("amongus.player_color" 		, "playerColor"		, base.DEC)
 
-ack_packets_field 		= ProtoField.bytes("amongus.ack_packets" 		, "acknowledgedPackets")
-
-amongus_protocol.fields = { packet_type_field, message_length_field, 
-							nonce_field, root_message_type_field, 
-							gameID_field, senderID_field,
-							gameData_type_field, RPC_type_field,
-							ack_packets_field }
+amongus_protocol.fields = { packet_type_field, message_length_field, nonce_field, root_message_type_field, gameID_field, gameData_type_field, RPC_type_field, player_color_field }
 
 
 
@@ -139,7 +148,7 @@ function amongus_protocol.dissector(buffer, pinfo, tree)
 	print("buffer(0,0): " .. buffer(0,1))
 
 	local packet_type_value = buffer(0,1):int()
-	subtree:add(packet_type_field,	buffer(0,1)):append_text(" (" .. name(packet_type, packet_type_value) ..")")
+	subtree:add(packet_type_field,	packet_type_value):append_text(" (" .. name(packet_type, packet_type_value) ..")")
 
 
 	-- If Packet Type: 	NORMAL
@@ -147,57 +156,43 @@ function amongus_protocol.dissector(buffer, pinfo, tree)
 
 	-- If Packet Type: 	RELIABLE  (TODO: MAY HAVE MORE THAN ONE MESSAGE PER PACKET)
 	if packet_type_value == packet_type['RELIABLE'] then
-		local nonce_value = buffer(1,2):int() -- TODO: fix/confirm bytes, since WS is currently bugged -- ID of the packet so it can be ACK'd
-		subtree:add(nonce_field, buffer(1,2))
+		local nonce_value = buffer(3,1):int() -- TODO: fix/confirm bytes, since WS is currently bugged -- ID of the packet so it can be ACK'd
+		subtree:add(nonce_field, nonce_value)
 		
 		local root_message_type_value = buffer(5,1):int()
-		subtree:add(root_message_type_field, buffer(5,1)):append_text(" (" .. name(root_message_type, root_message_type_value) .. ")")
+		subtree:add(root_message_type_field, root_message_type_value):append_text(" (" .. name(root_message_type, root_message_type_value) .. ")")
 	
 
 		-- If Root Message Type: GameData   TODO: these can also have more than one message
 		if root_message_type_value == root_message_type['GameData'] then
 			local gameID_value = buffer(6,1):int() -- TODO: confirm bytes, and decode the game code
-			subtree:add(gameID_field, buffer(6,1)):append_text(" (Not yet implemented.)")
-		
+			subtree:add(gameID_field, gameID_value):append_text(" (Not yet implemented.)")
+			
 			local gameData_type_value = buffer(12,1):int()
-			subtree:add(gameData_type_field, buffer(12,1)):append_text(" (" .. name(gameData_type, gameData_type_value) .. ")")
-			
-			
-			-- If GameData type is RPC
-			if gameData_type == gameData_type['RPC'] then
-				local senderID_value = buffer(14,1):int()
-				subtree:add(senderID, buffer(14,1))
-			
-				local RPC_type_value = buffer(14,1):int()
-				subtree:add(RPC_type_field, buffer(14,1)):append_text(" (" .. name(RPC_type, RPC_type_value) .. ")")
-				
-			
-			end
-		
+			subtree:add(gameData_type_field, gameData_type_value):append_text(" (" .. name(gameData_type, gameData_type_value) .. ")")
+
+            -- If GameData Type: RPC
+            if gameData_type_value == gameData_type['RPC'] then
+                local RPC_type_value = buffer(14,1):int()
+                subtree:add(RPC_type_field, RPC_type_value):append_text(" (" .. name(RPC_type, RPC_type_value) .. ")")
+
+                -- ...
+
+                -- If RPC Type: SetColor
+                if RPC_type_value == RPC_type['SetColor'] then
+                    local color_value = buffer(19,1):int()
+                    subtree:add(player_color_field, color_value):append_text(" (" .. name(player_color, color_value) .. ")")
+                end
+
+                -- ...
+            end
 		end
 		
+
+	end
 	
 	-- If Packet Type: HELLO
-	
-		--elseif packet_type_value == packet_type['HELLO']
-
-
-	-- If Packet type: DISCONNECT
-
-
-	-- If Packet type: ACK
-	elseif packet_type_value == packet_type['ACK'] then
-		local nonce_value = buffer(1,2):int()
-		subtree:add(nonce_field, buffer(1,2))
-		
-		local ack_packets_value = buffer(3,1):int()
-		subtree:add(ack_packets_field, buffer(3,1))
-		
-		
-	-- If Packet type: FRAGMENT
-	
-	end  -- if packet_type
-end -- main function
+end
 
 
 
